4a95cd378ffe9b631b018649ee1c2f41d8975b11
Rick Levy <ricklevy21@gmail.com>
2020-06-13 12:39:01 -0600

    updated delete post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -50,7 +50,7 @@ module.exports = function(app){
           }); 
           fs.writeFile("db/db.json", JSON.stringify(noteData), function(err){
             if (err) throw err;
-            
+            res.end(console.log("note deleted"));
           })
         });
         //res.send(data);
